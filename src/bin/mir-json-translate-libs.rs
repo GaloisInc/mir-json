7f6e6c382aafab4ffafde7a34ba2c018740a55aa
Bretton <qsctr@users.noreply.github.com>
2025-04-01 22:08:20 -0700

    Automate translate_libs

---
+++ src/bin/mir-json-translate-libs.rs
@@ -0,0 +1,599 @@
+#![feature(rustc_private)]
+
+extern crate cargo_metadata;
+extern crate rustc_driver;
+extern crate rustc_session;
+extern crate serde;
+extern crate shell_escape;
+
+use std::{
+    collections::HashMap,
+    convert::TryInto,
+    env, fmt, fs,
+    io::BufReader,
+    os::unix,
+    process::{Command, Stdio},
+};
+
+use cargo_metadata::{
+    camino::{Utf8Path, Utf8PathBuf},
+    Edition, Message, PackageId, Target,
+};
+use serde::Deserialize;
+use shell_escape::escape;
+
+const EMPTY_PROJECT_NAME: &str = "mir-json-translate-libs-empty-project";
+
+#[derive(Deserialize)]
+struct UnitGraph {
+    version: u32,
+    units: Vec<UnitGraphUnit>,
+    roots: Vec<usize>,
+}
+
+#[derive(Deserialize)]
+struct UnitGraphUnit {
+    pkg_id: PackageId,
+    target: Target,
+    dependencies: Vec<UnitGraphDependency>,
+}
+
+#[derive(Clone, Deserialize)]
+struct UnitGraphDependency {
+    index: usize,
+    extern_crate_name: CrateName,
+}
+
+mod crate_name {
+    use std::fmt;
+
+    use serde::Deserialize;
+
+    #[derive(Clone, Deserialize, PartialEq)]
+    pub struct CrateName(String);
+
+    impl From<&str> for CrateName {
+        fn from(name: &str) -> Self {
+            Self(name.replace("-", "_"))
+        }
+    }
+
+    impl From<CrateName> for String {
+        fn from(crate_name: CrateName) -> String {
+            crate_name.0
+        }
+    }
+
+    impl fmt::Display for CrateName {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            self.0.fmt(f)
+        }
+    }
+}
+use crate_name::CrateName;
+
+struct CustomUnitGraph {
+    units: Vec<CustomUnit>,
+    roots: Vec<usize>,
+}
+
+struct CustomUnit {
+    target: CustomTarget,
+    dependencies: Vec<UnitGraphDependency>,
+}
+
+enum CustomTarget {
+    TargetLib(CustomTargetLib),
+    HostDep,
+    FinalBin,
+}
+
+#[derive(Clone)]
+struct CustomTargetLib {
+    crate_name: CrateName,
+    src_path: Utf8PathBuf,
+    edition: Edition,
+    linked_libs: Vec<Utf8PathBuf>,
+    linked_paths: Vec<Utf8PathBuf>,
+    cfgs: Vec<String>,
+    env: Vec<(String, String)>,
+}
+
+impl CustomTarget {
+    fn is_target_lib_with_crate_name(&self, crate_name: &CrateName) -> bool {
+        match self {
+            Self::TargetLib(lib) if lib.crate_name == *crate_name => true,
+            _ => false,
+        }
+    }
+}
+
+struct MirJsonLib {
+    target: CustomTargetLib,
+    dependencies: Vec<CrateName>,
+}
+
+impl CustomUnitGraph {
+    fn get_unit_as_dep(&self, crate_name: CrateName) -> UnitGraphDependency {
+        UnitGraphDependency {
+            index: self
+                .units
+                .iter()
+                .position(|u| {
+                    u.target.is_target_lib_with_crate_name(&crate_name)
+                })
+                .unwrap_or_else(|| {
+                    panic!("library {} should be in unit graph", crate_name)
+                }),
+            extern_crate_name: crate_name,
+        }
+    }
+
+    fn get_unit_mut(&mut self, crate_name: &CrateName) -> &mut CustomUnit {
+        self.units
+            .iter_mut()
+            .find(|u| u.target.is_target_lib_with_crate_name(crate_name))
+            .unwrap_or_else(|| {
+                panic!("library {} should be in unit graph", crate_name)
+            })
+    }
+
+    fn push_unit_as_dep(
+        &mut self,
+        lib: CustomTargetLib,
+        dependencies: Vec<UnitGraphDependency>,
+    ) -> UnitGraphDependency {
+        UnitGraphDependency {
+            extern_crate_name: lib.crate_name.clone(),
+            index: self.push_unit(CustomUnit {
+                target: CustomTarget::TargetLib(lib),
+                dependencies,
+            }),
+        }
+    }
+
+    fn push_unit_as_root(&mut self, unit: CustomUnit) {
+        let index = self.push_unit(unit);
+        self.roots.push(index);
+    }
+
+    fn push_unit(&mut self, unit: CustomUnit) -> usize {
+        let index = self.units.len();
+        self.units.push(unit);
+        index
+    }
+
+    fn sequence_libs(&self) -> Vec<MirJsonLib> {
+        let mut result = Vec::new();
+        let mut visited: Vec<Option<bool>> = vec![None; self.units.len()];
+
+        fn visit(
+            units: &[CustomUnit],
+            result: &mut Vec<MirJsonLib>,
+            visited: &mut [Option<bool>],
+            i: usize,
+        ) -> bool {
+            match visited[i] {
+                Some(is_lib) => is_lib,
+                None => {
+                    let is_lib = match &units[i].target {
+                        CustomTarget::TargetLib(lib) => {
+                            let mut lib_deps = Vec::new();
+                            for dep in &units[i].dependencies {
+                                if visit(units, result, visited, dep.index) {
+                                    lib_deps
+                                        .push(dep.extern_crate_name.clone());
+                                }
+                            }
+                            result.push(MirJsonLib {
+                                target: lib.clone(),
+                                dependencies: lib_deps,
+                            });
+                            true
+                        }
+                        CustomTarget::HostDep => false,
+                        CustomTarget::FinalBin => {
+                            for dep in &units[i].dependencies {
+                                visit(units, result, visited, dep.index);
+                            }
+                            false
+                        }
+                    };
+                    visited[i] = Some(is_lib);
+                    is_lib
+                }
+            }
+        }
+
+        for root in &self.roots {
+            visit(&self.units, &mut result, &mut visited, *root);
+        }
+
+        result
+    }
+}
+
+struct CmdInvocation {
+    program: String,
+    args: Vec<String>,
+    env: Vec<(String, String)>,
+}
+
+impl CmdInvocation {
+    fn as_command(&self) -> Command {
+        let mut cmd = Command::new(&self.program);
+        cmd.args(&self.args)
+            .envs(self.env.iter().map(|(k, v)| (k, v)));
+        cmd
+    }
+}
+
+impl fmt::Display for CmdInvocation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for (k, v) in &self.env {
+            write!(f, "{}={} ", k, escape(v.into()))?;
+        }
+        write!(f, "{}", self.program)?;
+        for arg in &self.args {
+            write!(f, " {}", escape(arg.into()))?;
+        }
+        Ok(())
+    }
+}
+
+fn main() {
+    let target_triple = env::var("TARGET")
+        .unwrap_or_else(|_| rustc_session::config::host_triple().into());
+    let cwd: Utf8PathBuf = env::current_dir()
+        .expect("cwd should be accessible")
+        .try_into()
+        .expect("cwd should be UTF-8");
+    let custom_sources_dir = match env::var_os("MIR_JSON_RUST_SOURCES_PATH") {
+        Some(path) => fs::canonicalize(path)
+            .expect("MIR_JSON_RUST_SOURCES_PATH should be a valid path")
+            .try_into()
+            .expect("MIR_JSON_RUST_SOURCES_PATH should be UTF-8"),
+        None => cwd.join("libs"),
+    };
+    let generate_only = env::args().skip(1).any(|arg| arg == "--generate");
+
+    eprintln!("Querying paths...");
+    let rlibs_sysroot = cwd.join("rlibs_real");
+    let rlibs_symlink = cwd.join("rlibs");
+    let rlibs_dir = Command::new("rustc")
+        .arg("--target")
+        .arg(&target_triple)
+        .arg("--print")
+        .arg("target-libdir")
+        .arg("--sysroot")
+        .arg(&rlibs_sysroot)
+        .output()
+        .expect("rustc --print target-libdir should run")
+        .stdout;
+    let rlibs_dir: &Utf8Path = std::str::from_utf8(&rlibs_dir)
+        .expect("rustc target-libdir should be UTF-8")
+        .trim_end()
+        .into();
+
+    eprintln!("Creating empty cargo package...");
+    let empty_project_path = cwd.join(EMPTY_PROJECT_NAME);
+    let _ = fs::remove_dir_all(&empty_project_path);
+    let cargo_new_status = Command::new("cargo")
+        .arg("new")
+        .arg(&empty_project_path)
+        .arg("--vcs")
+        .arg("none")
+        .status()
+        .expect("cargo new should run");
+    if !cargo_new_status.success() {
+        panic!(
+            "cargo new {} exited with {}",
+            empty_project_path, cargo_new_status
+        );
+    }
+
+    eprintln!("Running cargo test...");
+    let mut cargo_test_child =
+        cargo_test_cmd(&empty_project_path, &target_triple)
+            .arg("--message-format")
+            .arg("json")
+            .arg("--no-run")
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("cargo test should run");
+    let cargo_test_child_stdout = cargo_test_child
+        .stdout
+        .take()
+        .expect("cargo test stdout should be captured");
+    let cargo_test_child_stdout = BufReader::new(cargo_test_child_stdout);
+    let mut artifact_outputs = HashMap::new();
+    let mut build_script_outputs = HashMap::new();
+    for msg in Message::parse_stream(cargo_test_child_stdout) {
+        match msg.expect("reading cargo test output should succeed") {
+            Message::CompilerArtifact(art) => {
+                if art
+                    .target
+                    .crate_types
+                    .iter()
+                    .all(|t| t != "bin" && t != "proc-macro")
+                {
+                    artifact_outputs.insert(art.package_id.clone(), art);
+                }
+            }
+            Message::BuildScriptExecuted(bs) => {
+                build_script_outputs.insert(bs.package_id.clone(), bs);
+            }
+            _ => {}
+        }
+    }
+    let cargo_test_child_status =
+        cargo_test_child.wait().expect("cargo test should have run");
+    if !cargo_test_child_status.success() {
+        panic!("cargo test exited with {}", cargo_test_child_status);
+    }
+
+    eprintln!("Running cargo --unit-graph...");
+    let unit_graph = cargo_test_cmd(&empty_project_path, &target_triple)
+        .arg("-Z")
+        .arg("unstable-options")
+        .arg("--unit-graph")
+        .output()
+        .expect("cargo --unit-graph should run")
+        .stdout;
+    let unit_graph: UnitGraph = serde_json::from_slice(&unit_graph)
+        .expect("cargo --unit-graph output should match JSON schema");
+    if unit_graph.version != 1 {
+        panic!(
+            "expecting version 1, got version {} of cargo --unit-graph output",
+            unit_graph.version
+        );
+    }
+
+    eprintln!("Modifying unit graph...");
+    let convert_unit = |unit: UnitGraphUnit| CustomUnit {
+        target: {
+            if unit.target.is_custom_build()
+                || unit.target.crate_types.iter().any(|t| t == "proc-macro")
+            {
+                CustomTarget::HostDep
+            } else if unit.target.crate_types.iter().any(|t| t == "bin") {
+                CustomTarget::FinalBin
+            } else {
+                let artifact =
+                    artifact_outputs.get(&unit.pkg_id).unwrap_or_else(|| {
+                        panic!(
+                            "library {} should have a compiler artifact",
+                            unit.pkg_id
+                        )
+                    });
+                let orig_pkg_dir = artifact
+                    .manifest_path
+                    .parent()
+                    .expect("manifest path should be a file");
+                let rel_src_path =
+                    unit.target.src_path.strip_prefix(orig_pkg_dir).expect(
+                        "source file should be inside directory of Cargo.toml",
+                    );
+                let src_path = [
+                    custom_sources_dir.as_path(),
+                    Utf8Path::new(&unit.target.name),
+                    rel_src_path,
+                ]
+                .iter()
+                .collect();
+                let (linked_libs, linked_paths, cfgs, env) =
+                    match build_script_outputs.remove(&unit.pkg_id) {
+                        Some(bs) => {
+                            (bs.linked_libs, bs.linked_paths, bs.cfgs, bs.env)
+                        }
+                        None => (vec![], vec![], vec![], vec![]),
+                    };
+                CustomTarget::TargetLib(CustomTargetLib {
+                    crate_name: unit.target.name.as_str().into(),
+                    src_path,
+                    edition: unit.target.edition,
+                    linked_libs,
+                    linked_paths,
+                    cfgs: artifact
+                        .features
+                        .iter()
+                        .map(|f| format!("feature=\"{f}\""))
+                        .chain(cfgs)
+                        .collect(),
+                    env,
+                })
+            }
+        },
+        dependencies: unit.dependencies,
+    };
+
+    let mut custom_graph = CustomUnitGraph {
+        units: unit_graph.units.into_iter().map(convert_unit).collect(),
+        roots: unit_graph.roots,
+    };
+
+    let dep_core = custom_graph.get_unit_as_dep("core".into());
+    let dep_compiler_builtins =
+        custom_graph.get_unit_as_dep("compiler_builtins".into());
+    let dep_std = custom_graph.get_unit_as_dep("std".into());
+
+    let dep_crucible = custom_graph.push_unit_as_dep(
+        CustomTargetLib {
+            crate_name: "crucible".into(),
+            src_path: [
+                custom_sources_dir.as_path(),
+                "crucible".into(),
+                "lib.rs".into(),
+            ]
+            .iter()
+            .collect(),
+            edition: Edition::E2021,
+            linked_libs: vec![],
+            linked_paths: vec![],
+            cfgs: vec![],
+            env: vec![],
+        },
+        vec![dep_compiler_builtins.clone(), dep_core.clone()],
+    );
+
+    custom_graph
+        .get_unit_mut(&"alloc".into())
+        .dependencies
+        .push(dep_crucible.clone());
+
+    custom_graph.push_unit_as_root(CustomUnit {
+        target: CustomTarget::TargetLib(CustomTargetLib {
+            crate_name: "int512".into(),
+            src_path: custom_sources_dir.join("int512.rs"),
+            edition: Default::default(),
+            linked_libs: vec![],
+            linked_paths: vec![],
+            cfgs: vec![],
+            env: vec![],
+        }),
+        dependencies: vec![dep_core.clone(), dep_compiler_builtins.clone()],
+    });
+
+    custom_graph.push_unit_as_root(CustomUnit {
+        target: CustomTarget::TargetLib(CustomTargetLib {
+            crate_name: "bytes".into(),
+            src_path: custom_sources_dir.join("bytes.rs"),
+            edition: Edition::E2021,
+            linked_libs: vec![],
+            linked_paths: vec![],
+            cfgs: vec![],
+            env: vec![],
+        }),
+        dependencies: vec![
+            dep_core.clone(),
+            dep_std.clone(),
+            dep_compiler_builtins.clone(),
+            dep_crucible,
+        ],
+    });
+
+    custom_graph.push_unit_as_root(CustomUnit {
+        target: CustomTarget::TargetLib(CustomTargetLib {
+            crate_name: "byteorder".into(),
+            src_path: [
+                custom_sources_dir.as_path(),
+                "byteorder".into(),
+                "src".into(),
+                "lib.rs".into(),
+            ]
+            .iter()
+            .collect(),
+            edition: Edition::E2021,
+            linked_libs: vec![],
+            linked_paths: vec![],
+            cfgs: vec!["feature=\"std\"".into()],
+            env: vec![],
+        }),
+        dependencies: vec![dep_core, dep_std, dep_compiler_builtins],
+    });
+
+    eprintln!("Setting up sysroot...");
+    for subdir in ["bin", "etc", "lib", "libexec", "share"] {
+        let new_dir = rlibs_sysroot.join(subdir);
+        if generate_only {
+            println!("mkdir -p {}", escape(new_dir.into_string().into()));
+        } else {
+            fs::create_dir_all(new_dir).unwrap_or_else(|err| {
+                panic!(
+                    "creating sysroot {} directory should succeed: {}",
+                    subdir, err
+                );
+            });
+        }
+    }
+    if generate_only {
+        let rlibs_symlink = escape(rlibs_symlink.into_string().into());
+        let rlibs_dir = escape(rlibs_dir.as_str().into());
+        println!(
+            "if [ ! -d {} ]; then ln -s {} {}; fi",
+            rlibs_symlink, rlibs_dir, rlibs_symlink
+        );
+    } else if !rlibs_symlink.is_dir() {
+        unix::fs::symlink(rlibs_dir, rlibs_symlink)
+            .expect("creating rlibs symlink should succeed");
+    }
+
+    let mir_json_invocations =
+        custom_graph
+            .sequence_libs()
+            .into_iter()
+            .map(|lib| CmdInvocation {
+                program: "mir-json".into(),
+                args: {
+                    let mut args = vec![
+                        lib.target.src_path.into(),
+                        "--edition".into(),
+                        lib.target.edition.to_string(),
+                        "--crate-name".into(),
+                        lib.target.crate_name.into(),
+                    ];
+                    for cfg in lib.target.cfgs {
+                        args.push("--cfg".into());
+                        args.push(cfg);
+                    }
+                    for dep in lib.dependencies {
+                        args.push("--extern".into());
+                        args.push(format!(
+                            "{}={}",
+                            dep,
+                            rlibs_dir.join(format!("lib{}.rlib", dep))
+                        ));
+                    }
+                    args.push("--target".into());
+                    args.push(target_triple.clone());
+                    args.push("-L".into());
+                    args.push(rlibs_dir.to_string());
+                    args.push("--out-dir".into());
+                    args.push(rlibs_dir.to_string());
+                    args.push("--crate-type".into());
+                    args.push("rlib".into());
+                    args.push("--remap-path-prefix".into());
+                    args.push(format!("{}=.", cwd));
+                    for linked_path in lib.target.linked_paths {
+                        args.push("-L".into());
+                        args.push(linked_path.into());
+                    }
+                    for linked_lib in lib.target.linked_libs {
+                        args.push("-l".into());
+                        args.push(linked_lib.into());
+                    }
+                    args
+                },
+                env: lib.target.env,
+            });
+
+    if generate_only {
+        eprintln!("Generating translation steps...");
+        for inv in mir_json_invocations {
+            println!("{}", inv);
+        }
+    } else {
+        for inv in mir_json_invocations {
+            eprintln!("Executing: {}", inv);
+            let status =
+                inv.as_command().status().expect("mir-json should run");
+            if !status.success() {
+                panic!("mir-json exited with {}", status);
+            }
+        }
+    }
+
+    eprintln!("Removing empty cargo package...");
+    let _ = fs::remove_dir_all(empty_project_path);
+}
+
+fn cargo_test_cmd(cwd: &Utf8Path, target_triple: &str) -> Command {
+    let mut cmd = Command::new("cargo");
+    cmd.current_dir(cwd)
+        .arg("test")
+        .arg("-Z")
+        .arg("build-std")
+        .arg("--target")
+        .arg(target_triple);
+    cmd
+}
