645d16532541e48e1a3388f63cbc8df0b5149e6f
Stuart Pernsteiner <spernsteiner@galois.com>
2025-04-17 15:37:17 -0700

    rustup: libs: patch is_null to use crucible::ptr::compare_usize

---
+++ libs-new/Patches.md
@@ -0,0 +1,25 @@
+# Patching the Rust standard library
+
+This directory bundles a copy of the Rust standard library with various patches
+applied in certain key places to make the resulting code easier for Crucible to
+handle. These patches must be applied every time that the bundled Rust standard
+library is updated. Moreover, this code often looks quite different each time
+between Rust versions, so applying the patches is rarely as straightforward as
+running `git apply`.
+
+As a compromise, this document contains high-level descriptions of each type of
+patch that we apply, along with rationale for why the patch is necessary. The
+intent is that this document can be used in conjunction with `git blame` to
+identify all of the code that was changed in each patch.
+
+* Use `crucible::ptr::compare_usize` for pointer-integer comparisons (last applied: April 17, 2025)
+
+  The `is_null` method on pointers works by casting the pointer to an integer
+  and comparing to zero.  However, crucible-mir doesn't support casting valid
+  pointers to integers (e.g. `&x as *const _ as usize` will always fail).  We
+  reimplement `is_null` in terms of `crucible::ptr::compare_usize(ptr, n)`,
+  which directly checks whether `ptr` is the result of casting `n` to a pointer
+  type.
+
+  The internal function `alloc::rc::is_dangling` is implemented similarly to
+  `is_null`, so we reimplement it in terms of `compare_usize` as well.
--- libs-new/core/src/ptr/const_ptr.rs
+++ libs-new/core/src/ptr/const_ptr.rs
@@ -1,5 +1,6 @@
 use super::*;
 use crate::cmp::Ordering::{Equal, Greater, Less};
+use crate::crucible;
 use crate::intrinsics::const_eval_select;
 use crate::mem::SizedTypeProperties;
 use crate::slice::{self, SliceIndex};
@@ -37,24 +38,7 @@ impl<T: ?Sized> *const T {
     #[inline]
     #[rustc_allow_const_fn_unstable(const_eval_select)]
     pub const fn is_null(self) -> bool {
-        // Compare via a cast to a thin pointer, so fat pointers are only
-        // considering their "data" part for null-ness.
-        let ptr = self as *const u8;
-        const_eval_select!(
-            @capture { ptr: *const u8 } -> bool:
-            // This use of `const_raw_ptr_comparison` has been explicitly blessed by t-lang.
-            if const #[rustc_allow_const_fn_unstable(const_raw_ptr_comparison)] {
-                match (ptr).guaranteed_eq(null_mut()) {
-                    Some(res) => res,
-                    // To remain maximally convervative, we stop execution when we don't
-                    // know whether the pointer is null or not.
-                    // We can *not* return `false` here, that would be unsound in `NonNull::new`!
-                    None => panic!("null-ness of this pointer cannot be determined in const context"),
-                }
-            } else {
-                ptr.addr() == 0
-            }
-        )
+        crucible::ptr::compare_usize(self, 0)
     }
 
     /// Casts to a pointer of another type.
