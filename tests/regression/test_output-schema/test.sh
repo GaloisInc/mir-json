a2cf700459a534e5c7c58192030667beb486ee2a
Ryan Scott <rscott@galois.com>
2025-10-02 18:44:57 -0400

    Test suite: Use `--print host-tuple` where possible
    This avoids some awkward `awk` hackery.

--- tests/regression/test_output-schema/test.sh
+++ tests/regression/test_output-schema/test.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 source "$(dirname "$0")/../../common.sh"
 
-expect_no_panic saw-rustc test.rs --target "$(rustc -vV | awk '/host:/ {print $2}')"
+expect_no_panic saw-rustc test.rs --target "$(rustc --print host-tuple)"
 
 expect_json_contains \
   '.fns[] | select(.name | test("::call_foo$"))' \
