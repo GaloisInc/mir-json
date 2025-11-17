bdf7f05a787a972143a0837c7fe7556f33d86ec5
chathhorn-galois <chathhorn@galois.com>
2025-11-17 08:54:26 -0600

    version.rs: include Rust toolchain version.

--- src/version.rs
+++ src/version.rs
@@ -1,6 +1,6 @@
 use crate::schema_ver::SCHEMA_VER;
 
-use std::env::Args;
+use std::env::{self, Args};
 
 /// Check if the arguments contain version flags ("--version" or "-V").
 pub fn has_flag(args: &mut Args) -> bool
@@ -12,9 +12,17 @@ pub fn has_flag(args: &mut Args) -> bool
     })
 }
 
-/// A string containing the mir-json and JSON schema versions.
+/// A string containing the mir-json version, JSON schema version, and the Rust
+/// toolchain version (if set).
 pub fn string() -> String {
-    format!("mir-json {} (JSON schema version {})", env!("CARGO_PKG_VERSION"), SCHEMA_VER)
+    let base = format!("mir-json {} (JSON schema version {})",
+        env!("CARGO_PKG_VERSION"), SCHEMA_VER);
+
+    if let Ok(toolchain) = env::var("RUSTUP_TOOLCHAIN") {
+        format!("{}\nRust toolchain {}", base, toolchain)
+    } else {
+        base
+    }
 }
 
 /// Print out the version string.
