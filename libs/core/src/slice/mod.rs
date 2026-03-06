cf04c48892b949abfe0b31f93fad9db1df42e05f
Eric Mertens <emertens@galois.com>
2026-03-06 14:13:42 -0800

    Revert "libs: Use `crucible_array_from_slice_hook` in `<[T]>::as_slice`"
    This reverts commit 888e49137bad4d3423239fed207ac9608547a52f.

--- libs/core/src/slice/mod.rs
+++ libs/core/src/slice/mod.rs
@@ -845,19 +845,15 @@ impl<T> [T] {
     #[inline]
     #[must_use]
     pub const fn as_array<const N: usize>(&self) -> Option<&[T; N]> {
-        #[inline(never)] // Keep the hook around even with optimizations applied
-        const fn crucible_array_from_slice_hook<T, const N: usize>(slice: &[T]) -> Option<&[T; N]> {
-            if slice.len() == N {
-                let ptr = slice.as_ptr().cast_array();
+        if self.len() == N {
+            let ptr = self.as_ptr().cast_array();
 
-                // SAFETY: The underlying array of a slice can be reinterpreted as an actual array `[T; N]` if `N` is not greater than the slice's length.
-                let me = unsafe { &*ptr };
-                Some(me)
-            } else {
-                None
-            }
+            // SAFETY: The underlying array of a slice can be reinterpreted as an actual array `[T; N]` if `N` is not greater than the slice's length.
+            let me = unsafe { &*ptr };
+            Some(me)
+        } else {
+            None
         }
-        crucible_array_from_slice_hook(self)
     }
 
     /// Gets a mutable reference to the slice's underlying array.
